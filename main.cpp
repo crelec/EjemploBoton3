3a77032e7b6aff1048220ca8ce09d9d999c4910d
César Augusto Romero Molano <ceroma_175@hotmail.com>
2024-05-14 21:08:09 +0000

    actualiza_salidas

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 
 int volatile contador=0;
-
+DigitalOut myled(PA_5);//D13 LED placa
 DigitalOut myled1(PA_10);//D2 = MSB
 DigitalOut myled2(PB_5);//D4 
 DigitalOut myled3(PB_4);//D5
@@ -12,18 +12,19 @@ InterruptIn myBoton(PC_13); //Uso pin PC_13 Boton tarjeta
 //InterruptIn myBoton(PB_3);  // Uso otro pin PB_3=D3 sin PullUp
 
 void Funcion_interrupcion(){
-    myled4 = (contador & 0x01);
-    myled3 = (contador & 0x02)>>1;
-    myled2 = (contador & 0x04)>>2;
+    myled1 = (contador & 0x01);
+    myled2 = (contador & 0x02)>>1;
+    myled3 = (contador & 0x04)>>2;
+    myled4 = (contador & 0x08)>>3;
     contador++;
 }
-
+// main() runs in its own thread in the OS
 int main()
 {
  myBoton.fall(&Funcion_interrupcion);
 
     while(1) {
-        myled1 = !myled1;
+        myled = !myled;
         ThisThread::sleep_for(500ms);
     }
 }
